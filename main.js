da7b8efd8a96e63a9c8e2a1d66e5a45a99250738
Eduardo Perez Moyano <pemoe90@gmail.com>
2018-03-06 08:55:25 +0100

    Initial commit

---
+++ main.js
@@ -0,0 +1,95 @@
+var controlador = {
+	imgSup: [],
+	imgInf: [],
+	imagenes: ["img/1.png", "img/2.png", "img/3.png" ,"img/4.png", "img/5.png", "img/6.png", "img/7.png", "img/8.png", "img/9.png"],
+	copiaImagenes: [],
+	imagenRepetida: "",
+	puntos: 0,
+};
+
+
+
+window.onload = function (){
+	buscarElementosImg();
+	colocarImagenesSuperiores();
+}
+
+function empezar(){
+	buscarElementosImg();
+	colocarImagenes();
+}
+
+function buscarElementosImg(){
+	var idSup = "Sup";
+	var idInf = "Inf";
+	var id = "";
+	if(controlador.imgSup.length == 0){
+		for(var i = 1; i<=5; i++){
+			//busco y guardo las imagenes superiores
+			id = idSup + i;
+			var elemento = document.getElementById(id);
+			controlador.imgSup.push(elemento);
+			
+			//busco y guardo las imagenes inferiores
+			id = idInf + i;
+			elemento = document.getElementById(id);
+			controlador.imgInf.push(elemento);
+		}
+	}
+	
+}
+
+function colocarImagenes(){
+	colocarImagenesSuperiores();
+}
+
+function copiarArraysImagenes(){
+	controlador.copiaImagenes.splice(0, controlador.copiaImagenes.length);
+	
+	for(i = 0; i < controlador.imagenes.length; i++){
+		var aux = controlador.imagenes[i];
+		controlador.copiaImagenes.push(aux);
+	}
+	
+}
+
+function colocarImagenesSuperiores(){
+	copiarArraysImagenes();
+	var elementoARepetir = Math.floor(Math.random() * 4);
+	for(i = 0; i < controlador.imgSup.length; i++){
+		var longitudCopiaImagenes = controlador.copiaImagenes.length - 1;
+		var aux = Math.floor(Math.random() * longitudCopiaImagenes);
+		controlador.imgSup[i].src = controlador.copiaImagenes[aux];
+		controlador.copiaImagenes.splice(aux, 1);
+		if(i == elementoARepetir){
+			controlador.imagenRepetida = controlador.imgSup[i].src;
+		}
+	}
+	
+	colocarImagenesInferiores();
+}
+
+function colocarImagenesInferiores(){
+	var elementoARepetir = Math.floor(Math.random() * 4);
+	for(var i = 0; i < controlador.imgInf.length; i++){
+		if(i == elementoARepetir){
+			controlador.imgInf[i].src = controlador.imagenRepetida;
+		}
+		else{
+			var longitudCopiaImagenes = controlador.copiaImagenes.length - 1;
+			var aux = Math.floor(Math.random() * longitudCopiaImagenes);
+			controlador.imgInf[i].src = controlador.copiaImagenes[aux];
+			controlador.copiaImagenes.splice(aux, 1);
+		}
+	}
+}
+
+function comprobarRepetido(imagen){
+	if(imagen.src == controlador.imagenRepetida){
+		controlador.puntos ++;
+	}
+	else{
+		controlador.puntos --;
+	}
+	empezar();
+}
